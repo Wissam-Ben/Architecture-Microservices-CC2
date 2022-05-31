a4057134f0e6bd5d5943aad13d87af4275c14055
Wissam-Ben <benadjaoud.wissam@gmail.com>
2022-05-31 22:03:53 +0200

    add Redis tools

---
+++ src/main/java/io/swagger/configuration/RedisConfiguration.java
@@ -0,0 +1,9 @@
+package io.swagger.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedisConfiguration {
+
+}
